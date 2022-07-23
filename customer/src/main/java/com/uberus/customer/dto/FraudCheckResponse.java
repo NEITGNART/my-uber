31963054d796e06ff4b1eadfd3db4fe55205ec15
Pham Tien <19127576@student.hcmus.edu.vn>
2022-07-24 00:28:42 +0700

    Update lecture 19

---
+++ customer/src/main/java/com/uberus/customer/dto/FraudCheckResponse.java
@@ -0,0 +1,4 @@
+package com.uberus.customer.dto;
+
+public record FraudCheckResponse(Boolean isFraudster) {
+}
--- customer/src/main/java/com/uberus/customer/service/CustomerService.java
+++ customer/src/main/java/com/uberus/customer/service/CustomerService.java
@@ -1,15 +1,25 @@
 package com.uberus.customer.service;
 
+import com.uberus.customer.dto.FraudCheckResponse;
 import com.uberus.customer.entity.Customer;
 import com.uberus.customer.dto.CustomerRegistrationRequest;
 import com.uberus.customer.repository.CustomerRepository;
+import lombok.NoArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 @Service
-public record CustomerService(CustomerRepository db) {
+public record CustomerService(
+        CustomerRepository db,
+        RestTemplate restTemplate
+) {
+
     public void registerCustomer(CustomerRegistrationRequest request) {
         var customer = Customer.builder().firstName(request.firstName()).lastName(request.lastName()).email(request.email()).build();
-        db.save(customer);
+        db.saveAndFlush(customer);
+        restTemplate.getForObject("http://localhost:8081/api/v1/fraud-check/{customerId}", FraudCheckResponse.class,  customer.getId());
+
+
     }
 }
 
